75cbaad32b395eeda5585d25c23f011086f9e9b1
Giraut <>
2021-12-14 14:43:26 +0200

    Populated the new repository for testing

---
+++ packages/update_rpm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+### This script should be run in the "packages" directory of the YUM / DNF
+### repository to update
+
+### Parameters
+# Email address for the GPG key
+EMAIL=$(C=base64\ -d;$C<<<Y205emEybHpRSEoxYm1KdmVDNWpiMjBLCg==|$C)
+
+
+
+### Main script
+# Check that we have at least one .rpm file in the current directory, to make
+# sure we're not being called from some other directory
+if ! ls *.rpm > /dev/null 2> /dev/null; then
+  echo "No .rpm files in the current directory"
+  exit
+fi
+
+# Sign the packages
+rpm --define "_gpg_name ${EMAIL}" --addsign *.rpm
+
+# Clear the current repodata metadata
+rm -r repodata
+
+# Create the repodata metadata
+createrepo_c .
+
+# Sign the repodata metadata
+gpg --default-key ${EMAIL} --detach-sign --armor repodata/repomd.xml
